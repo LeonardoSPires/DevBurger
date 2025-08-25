79793b52e2a0c48b1590b8f65ad97e19985fce99
Leonardo Pires <leopires1988@gmail.com>
2025-08-25 20:39:58 -0300

    Criado parte do routes.js, models e database.js

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import { v4 } from 'uuid';
+
+import User from './app/models/User.js';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.status(200).json({ message: 'Welcome to DevBurger API!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Leonardo Pires',
+    email: 'leo.pires@example.com',
+    password_hash: '123456',
+  });
+  return res.status(201).json(user);
 });
 
 export default routes;
