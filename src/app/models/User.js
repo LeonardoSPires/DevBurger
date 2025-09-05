97fe199057574092c897c509195f8fc1382e9faa
Leonardo Pires <leopires1988@gmail.com>
2025-09-05 15:40:31 -0300

    Fiz parte da configuração do login de usuário.

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,10 +1,13 @@
-import  Sequelize, { Model}  from "sequelize";
+import  Sequelize, { Model }  from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
     static init(sequelize) {
-        super.init({
+        super.init(
+            {
             name: Sequelize.STRING,
             email: Sequelize.STRING,
+            password: Sequelize.VIRTUAL,
             password_hash: Sequelize.STRING,
             admin: Sequelize.BOOLEAN,
         },
@@ -12,6 +15,18 @@ class User extends Model {
             sequelize,
         },
         );
+
+        this.addHook('beforeSave', async (user) => {
+            if (user.password) {
+                user.password_hash = await bcrypt.hash(user.password, 10);
+            }
+        });
+        
+        return this;
+    }
+
+    async checkPassword(password) {
+        return bcrypt.compare(password, this.password_hash);
     }
 }
 
