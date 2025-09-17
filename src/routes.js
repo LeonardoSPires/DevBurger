586b7fa22b993d1f2253404b3971d7168ab8421f
Leonardo Pires <leopires1988@gmail.com>
2025-09-17 13:43:59 -0300

    Refatoração do middleware de autenticação e criação do esquema de pedidos

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer.js';
-import authMiddleware from './middlewares/auth.js';
+import authMiddleware from './app/middlewares/auth.js';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
