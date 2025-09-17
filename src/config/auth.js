9364fe2aaa15442db207015944191840910d3e9f
Leonardo Pires <leopires1988@gmail.com>
2025-09-17 13:06:26 -0300

    Crie a conexão com o mongodb

--- src/config/auth.js
+++
@@ -1,4 +0,0 @@
-export default {
-  secret: '90d325ded9d8839d3dafa5c63e304e16',
-  expiresIn: '5d',
-};
--- src/database/index.js
+++ src/database/index.js
@@ -5,12 +5,14 @@ import configDatabase from "../config/database.js";
 import User from "../app/models/User.js";
 import Product from "../app/models/Product.js";
 import Category from "../app/models/Category.js";
+import mongoose from "mongoose";
 
 const models = [User, Product, Category];
 
 class Database {
     constructor() {
         this.init();
+        this.mongo();
     }
 
     init() {
@@ -18,6 +20,12 @@ class Database {
         models
         .map((model) => model.init(this.connection))
         .map((model) => model.associate && model.associate(this.connection.models));
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            'mongodb://localhost:27017/devburger',
+        )
     }
 }
 
